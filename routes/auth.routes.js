fecb044439d87fb4ed20b1699355ecf835ddd23e
aarestigue <108528013+aarestigue@users.noreply.github.com>
2022-08-10 11:19:43 +0100

    layout nav bar

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -85,6 +85,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
         // Bind the user to the session object
         
         req.session.user = user;
+        req.app.locals.loggedInUser = user
         res.redirect(`/${user.username}/profile`);
       })
       .catch((error) => {
@@ -153,9 +154,12 @@ router.post("/login", isLoggedOut, (req, res, next) => {
           });
         }
         req.session.user = user;
+        req.app.locals.loggedInUser = user
         // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
         return res.redirect("/");
       });
+      
+      
     })
 
     .catch((err) => {
